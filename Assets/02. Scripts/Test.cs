0627a45f03f24d836c9a424db3d8268320daec87
이승빈 <144720218+Pyorang@users.noreply.github.com>
2025-11-10 09:59:04 +0900

    Chore : Gemini Code Assist Test

---
+++ Assets/02. Scripts/Test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int centerpos = 4;
+
+    void Update()
+    {
+        if (centerpos == 4) {
+            transform.position = new Vector3(centerpos, 0, 0);
+        }
+    }
+}
+
