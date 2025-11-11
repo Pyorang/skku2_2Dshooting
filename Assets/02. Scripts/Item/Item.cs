652fc63cc20e5b5073a015fe9ad35bda53b1b6c9
이승빈 <144720218+Pyorang@users.noreply.github.com>
2025-11-11 14:41:12 +0900

    Feat : 아이템 획득 이펙트 구현

--- Assets/02. Scripts/Item/Item.cs
+++ Assets/02. Scripts/Item/Item.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public abstract class Item : MonoBehaviour
 {
+    [SerializeField] private GameObject _gainEffect;
     abstract protected void ProcessItemEffect(Collider2D collision);
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -9,6 +10,7 @@ public abstract class Item : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             ProcessItemEffect(collision);
+            Instantiate(_gainEffect, collision.transform);
             Destroy(gameObject);
         }
     }
