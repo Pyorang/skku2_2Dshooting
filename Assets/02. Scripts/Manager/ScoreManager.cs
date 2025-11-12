eac0662463f579327cbf40e9f2c926b4b15dbc50
이승빈 <144720218+Pyorang@users.noreply.github.com>
2025-11-12 13:03:04 +0900

    Feat : 최고 점수 기능 구현

--- Assets/02. Scripts/Manager/ScoreManager.cs
+++ Assets/02. Scripts/Manager/ScoreManager.cs
@@ -8,31 +8,24 @@ public class ScoreManager : MonoBehaviour
     // 응집도를 높이고, 필요한 것만 외부에 공개하는 것을 캡슐화라고 한다.
 
     [SerializeField] private Text _currentScoreTextUI;
+    [SerializeField] private Text _bestScoreTextUI;
     private int _currentScore = 0;
+    private int _bestScore = 0;
 
     private void Start()
     {
-        Refresh();
         Load();
     }
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            Save();
-        }
-        else if(Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            Load();
-        }
-    }
-
     public void AddScore(int score)
     {
         if (score <= 0) return;
 
         _currentScore += score;
+        
+        if(_currentScore >= _bestScore)
+        {
+            _bestScore = _currentScore;
+        }
 
         Refresh();
     }
@@ -40,24 +33,23 @@ public class ScoreManager : MonoBehaviour
     private void Refresh()
     {
         _currentScoreTextUI.text = $"현재 점수 : {_currentScore:N0}";
+        _bestScoreTextUI.text = $"최고 점수 : {_bestScore:N0}";
     }
 
     public void Save()
     {
-        // 유니티에서는 값을 저장할 때 'PlayerPrefs' 모듈을 씁니다.
-        // 저장 가능한 자료형은 : int, float, string
-        // 저장을 할 때는 저장할 이름(key)과 값(value)이 두형태로 저장을 해요.
+        if(_currentScore > _bestScore)
+        {
+            _bestScore = _currentScore;
+        }
 
-        // 저장 : Set
-        // 로드 : Get
-
-        PlayerPrefs.SetInt("score", _currentScore);
+        PlayerPrefs.SetInt("bestScore", _bestScore);
         Debug.Log("저장됐습니다");
     }
 
     private void Load()
     {
-        int score = PlayerPrefs.GetInt("score");
-        Debug.Log($"전판의 점수는 {score:N0}점입니다.");
+        _bestScore = PlayerPrefs.GetInt("bestScore", 0);
+        Refresh();
     }
 }
